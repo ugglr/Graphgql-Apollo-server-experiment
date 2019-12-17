4a154e1d3101de98998de36b5fddebaf025f99fa
Carl-W <carl.igelstroem@tiket.com>
2019-12-17 15:15:08 +0800

    added schemas etc.

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -8,12 +8,49 @@ const typeDefs = gql`
     me: User
   }
 
-  type Lanuch {
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+  }
+
+  type Launch {
     id: ID!
     site: String
     mission: Mission
     rocket: Rocket
     isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type Mission {
+    name: String!
+    missionPatch(size: PatchSize): String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
   }
 `;
 
